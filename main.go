98cd3836e411dec0ed623e60a568d1eaad83e559
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2018-09-22 12:04:38 -0300

    Use ParseArgs on main

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/rafaelhenrique/go-reminder/parser"
+)
+
 // "github.com/sqweek/dialog"
 
 // func showMessage(msg string) {
 // 	dialog.Message("%s", msg).Title("Reminder").Info()
 // }
 
-func main() {}
+func main() {
+	parsed := parser.ParseArgs(os.Args)
+	fmt.Println(parsed)
+}
